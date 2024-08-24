33c58b21d482dead6f78c9c08ab07f4619bb3d80
Papop Lekhapanyaporn <75293379+Retaehc-pop@users.noreply.github.com>
2024-08-25 00:30:10 +0200

    remove build directory

--- build/CMakeFiles/cclock.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for cclock.
-# This may be replaced when dependencies are built.
--- build/FTXUI/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.o.d
+++
@@ -1,4 +0,0 @@
-FTXUI/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.o: \
- /home/papop/project/tui/clock/FTXUI/src/ftxui/dom/flexbox_config.cpp \
- /usr/include/stdc-predef.h \
- /home/papop/project/tui/clock/FTXUI/include/ftxui/dom/flexbox_config.hpp
